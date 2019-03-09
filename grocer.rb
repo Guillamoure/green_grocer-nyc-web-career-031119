aeb1f2dc9ed7641eb6e623917502c4666d4a4bc6
cool-invocation-5581 <jhilscheriv@gmail.com>
2019-03-09 17:28:54 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -25,7 +25,7 @@ def apply_coupons(cart, coupons)
     if cart.keys.include?sale[:item]
       if cart[sale[:item]][:count] >= sale[:num] && cart[sale[:item]][:clearance] == true
         cart[sale[:item]][:count] = cart[sale[:item]][:count] - sale[:num]
-        with_coupon = cart[sale[:item]] + " W/COUPON"
+        with_coupon = sale[:item] + " W/COUPON"
         cart[with_coupon] = {}
         cart[with_coupon][:price] = sale[:cost]
         cart[with_coupon][:clearance] = "true"
